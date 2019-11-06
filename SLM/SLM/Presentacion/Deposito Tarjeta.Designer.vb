120939bf016aed64827bc60283c329a03c0b3064
software2 <software2@PC-CACX>
2019-11-06 13:17:03 -0600

    ejemplo

--- SLM/SLM/Presentacion/Deposito Tarjeta.Designer.vb
+++ SLM/SLM/Presentacion/Deposito Tarjeta.Designer.vb
@@ -22,9 +22,16 @@ Partial Class Deposito_Tarjeta
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'Deposito_Tarjeta
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Name = "Deposito_Tarjeta"
         Me.Text = "Deposito_Tarjeta"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
--- SLM/SLM/Presentacion/Deposito Tarjeta.vb
+++ SLM/SLM/Presentacion/Deposito Tarjeta.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Deposito_Tarjeta
-
+    Private Sub Deposito_Tarjeta_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Close()
+    End Sub
 End Class
