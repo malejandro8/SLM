ded5534d1f0e38af098a206dc498107583ef7ca2
Software 3 <Software 3@DESKTOP-IMTN6BO>
2019-11-06 13:03:42 -0600

    Cambio2

--- SLM/SLM/Presentacion/TipoDeposito.Designer.vb
+++ SLM/SLM/Presentacion/TipoDeposito.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class TipoDeposito
+Partial Class frmTipoDeposito
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -22,9 +22,16 @@ Partial Class TipoDeposito
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'frmTipoDeposito
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Name = "frmTipoDeposito"
         Me.Text = "TipoDeposito"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
--- SLM/SLM/Presentacion/TipoDeposito.vb
+++ SLM/SLM/Presentacion/TipoDeposito.vb
@@ -1,3 +1,3 @@
-﻿Public Class TipoDeposito
+﻿Public Class frmTipoDeposito
 
 End Class
